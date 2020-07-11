7c6f545e5d2e2542eb52f32048c413eca41d4b71
asaouda <alinesaouda@gmail.com>
2020-07-10 23:14:25 -0300

    Implementando: Environments e ProjectsEnvironments - repository, Services, Interface e controller

---
+++ ErrorMonitoring.Dominio/Interfaces/IProjectsEnvironmentsService.cs
@@ -0,0 +1,16 @@
+﻿using ErrorMonitoring.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErrorMonitoring.Dominio.Interfaces
+{
+    public interface IProjectsEnvironmentsService
+    {
+        IList<ProjectsEnvironments> ProjectsEnvironments();
+        ProjectsEnvironments GetById(int id);
+        ProjectsEnvironments Save(ProjectsEnvironments projectsEnvironment);
+        ProjectsEnvironments Update(ProjectsEnvironments projectsEnvironment);
+        bool Delete(int id);
+    }
+}
