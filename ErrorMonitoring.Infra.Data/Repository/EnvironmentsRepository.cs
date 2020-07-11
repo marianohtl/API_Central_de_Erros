8a4f700175d2701c19d3b9ce3724a5c62ca30f96
asaouda <alinesaouda@gmail.com>
2020-07-10 23:25:03 -0300

    Repository Environments e ProjectsEnvironments

---
+++ ErrorMonitoring.Infra.Data/Repository/EnvironmentsRepository.cs
@@ -0,0 +1,63 @@
+﻿using ErrorMonitoring.Dominio.Entidades;
+using ErrorMonitoring.Dominio.Interfaces;
+using ErrorMonitoring.Infra.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ErrorMonitoring.Infra.Data.Repository
+{
+    public class EnvironmentsRepository : IEnvironmentsRepository
+    {
+        private readonly ApiContext _apiContext;
+        public EnvironmentsRepository(ApiContext apiContext)
+        {
+            this._apiContext = apiContext;
+        }
+
+        public IEnumerable<Environments> Get()
+        {
+            return _apiContext.Environments;
+        }
+
+        public Environments GetById(int id)
+        {
+            return _apiContext.Environments.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public Environments Save(Environments environment)
+        {
+            var state = environment.Id == 0 ? EntityState.Added : EntityState.Modified;
+            _apiContext.Entry(environment).State = state;
+            _apiContext.Add(environment);
+            _apiContext.SaveChanges();
+            return environment;
+        }
+
+        public Environments Update(Environments environment)
+        {
+            var _environment = _apiContext.Environments.Where(x => x.Id == environment.Id).FirstOrDefault();
+            if (_environment != null)
+            {
+                _environment.EName = environment.EName;
+                _apiContext.Entry(_environment).State = EntityState.Modified;
+                _apiContext.SaveChanges();
+            }
+            return _environment;
+
+        }
+
+        public bool Delete(int id)
+        {
+            var _environment = _apiContext.Environments.Where(x => x.Id == id).FirstOrDefault();
+            if (_environment != null)
+            {
+                _apiContext.Entry(_environment).State = EntityState.Deleted;
+                _apiContext.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ErrorMonitoring.Infra.Data/Repository/ProjectsEnvironmentsRepository.cs
@@ -0,0 +1,64 @@
+﻿using ErrorMonitoring.Dominio.Entidades;
+using ErrorMonitoring.Dominio.Interfaces;
+using ErrorMonitoring.Infra.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ErrorMonitoring.Infra.Data.Repository
+{
+    public class ProjectsEnvironmentsRepository: IProjectsEnvironmentsRepository
+    {
+        private readonly ApiContext _apiContext;
+        public ProjectsEnvironmentsRepository(ApiContext apiContext)
+        {
+            this._apiContext = apiContext;
+        }
+
+        public IEnumerable<ProjectsEnvironments> Get()
+        {
+            return _apiContext.ProjectsEnvironments;
+        }
+
+        public ProjectsEnvironments GetById(int id)
+        {
+            return _apiContext.ProjectsEnvironments.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public ProjectsEnvironments Save(ProjectsEnvironments projectsenvironment)
+        {
+            var state = projectsenvironment.Id == 0 ? EntityState.Added : EntityState.Modified;
+            _apiContext.Entry(projectsenvironment).State = state;
+            _apiContext.Add(projectsenvironment);
+            _apiContext.SaveChanges();
+            return projectsenvironment;
+        }
+
+        public ProjectsEnvironments Update(ProjectsEnvironments projectsenvironment)
+        {
+            var _projectsenvironment = _apiContext.ProjectsEnvironments.Where(x => x.Id == projectsenvironment.Id).FirstOrDefault();
+            if (_projectsenvironment != null)
+            {
+                _projectsenvironment.Environment = projectsenvironment.Environment;
+                _apiContext.Entry(_projectsenvironment).State = EntityState.Modified;
+                _apiContext.SaveChanges();
+            }
+            return _projectsenvironment;
+
+        }
+
+        public bool Delete(int id)
+        {
+            var _projectsenvironment = _apiContext.ProjectsEnvironments.Where(x => x.Id == id).FirstOrDefault();
+            if (_projectsenvironment != null)
+            {
+                _apiContext.Entry(_projectsenvironment).State = EntityState.Deleted;
+                _apiContext.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
